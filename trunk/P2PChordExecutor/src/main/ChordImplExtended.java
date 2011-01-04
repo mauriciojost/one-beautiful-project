fc846f68823da00e176c8b83f814827d3e9f97b7
mauriciojost <mauriciojost@d51e7b54-0ae6-4b57-6df4-149d15c66451>
2011-01-04 23:56:04 +0000

    [code] We need to find out whether a node correspond to us or not.

--- trunk/P2PChordExecutor/src/main/ChordImplExtended.java
+++ trunk/P2PChordExecutor/src/main/ChordImplExtended.java
@@ -3,6 +3,7 @@ package main;
 
 import de.uniba.wiai.lspi.chord.service.impl.*;
 import de.uniba.wiai.lspi.chord.com.Entry;
+import de.uniba.wiai.lspi.chord.data.ID;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Set;
@@ -11,14 +12,34 @@ public class ChordImplExtended extends ChordImpl {
 
     public String printMyEntries(){
         String ret = "";
-        Collection set = this.entries.getValues();
+        Collection<Set> set = this.entries.getValues();
 
-        Iterator<Entry> i = set.iterator();
+        Iterator<Set> i = set.iterator();
         System.out.println("Printing " + set.size() + " elements.");
         while(i.hasNext()){
-            ret = ret + i.next();
+            Set s = i.next();
+            Iterator <Entry> j = s.iterator();
+            while(j.hasNext()){
+                Entry e = j.next();
+                if (itBelongsToMe(e.getId())){
+                    ret = ret +"\n"+ e.toString();
+                }else{
+                    ret = ret +"\n  ("+ e.toString() + ")";
+                }
+            }
+            
+            
         }
         System.out.println(ret + "\nPrinting done.");
         return ret; 
+    }
+
+    public boolean itBelongsToMe(ID key){
+        return (!itBelongsToMyPredecessor(key));
+    }
+
+    public boolean itBelongsToMyPredecessor(ID key){
+        return ((this.getPredecessorID().compareTo(key)>=0) /* Npred > k */
+        );
     }
 }
--- trunk/openchord/compile.bat
+++ trunk/openchord/compile.bat
@@ -1,3 +1,4 @@
+PATH=E:\A - Polytech\Pear to Pear\project\apache-ant-1.8.2\bin;C:\Archivos de programa\Java\jdk1.6.0_01\bin
 ant compile
 ant dist
 pause
