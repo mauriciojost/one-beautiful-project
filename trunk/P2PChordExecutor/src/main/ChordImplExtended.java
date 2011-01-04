58e678c0669aaa3e47bfb99dcadd712f42e3105b
mauriciojost <mauriciojost@d51e7b54-0ae6-4b57-6df4-149d15c66451>
2011-01-04 22:48:11 +0000

    [code] Some more changes to find out our items. There are replicas so a mechanism to get OUR item to execute must be implemented.

--- trunk/P2PChordExecutor/src/main/ChordImplExtended.java
+++ trunk/P2PChordExecutor/src/main/ChordImplExtended.java
@@ -1,12 +1,24 @@
 
 package main;
 
-import de.uniba.wiai.lspi.chord.service.impl.ChordImpl;
-
+import de.uniba.wiai.lspi.chord.service.impl.*;
+import de.uniba.wiai.lspi.chord.com.Entry;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
 
 public class ChordImplExtended extends ChordImpl {
 
-    public void getENtries(){
-        
+    public String printMyEntries(){
+        String ret = "";
+        Collection set = this.entries.getValues();
+
+        Iterator<Entry> i = set.iterator();
+        System.out.println("Printing " + set.size() + " elements.");
+        while(i.hasNext()){
+            ret = ret + i.next();
+        }
+        System.out.println(ret + "\nPrinting done.");
+        return ret; 
     }
 }
