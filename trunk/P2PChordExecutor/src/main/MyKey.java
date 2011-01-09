447d0ad54d135cf410f90a8e8dcd353d4a594bfe
mauriciojost <mauriciojost@d51e7b54-0ae6-4b57-6df4-149d15c66451>
2011-01-09 11:35:51 +0000

    [code] Solved a problem with ID and keys. I was confused about their meaning. Since openchord does not allow us to get a value from the ID (but from the key) I have changed it (it avoids applying the hash function to the thing I give). Working!

--- trunk/P2PChordExecutor/src/main/MyKey.java
+++ trunk/P2PChordExecutor/src/main/MyKey.java
@@ -1,45 +1,19 @@
 package main;
-import de.uniba.wiai.lspi.chord.data.ID;
+
 import de.uniba.wiai.lspi.chord.service.*;
-
 public class MyKey implements Key{
-    public final static int KEY_SIZE = 20;
-    private byte[] key;
-
-   
-    public MyKey(byte[] rawkey){
-        key = rawkey; 
-    }
+    private String key;
     
     public MyKey(String str_key){
-        this.key = new byte[KEY_SIZE];
-        if (str_key.length() >= KEY_SIZE){
-            System.out.println("Key string big enough.");
-            for (int i=0; i<KEY_SIZE;i++){
-                this.key[i] = (byte)str_key.charAt(i);
-            }
-        }else{
-            System.out.println("Key strign not big enough. Completing with zeros...");
-            for (int i=0; i<KEY_SIZE;i++){
-                this.key[i] = (i<str_key.length()?(byte)str_key.charAt(i):0);
-            }
-        }
-        
+        key = str_key;
     }
+    
     public byte[] getBytes() {
-        return key;
-    }
-
-    public ID getEquivalentID(){
-        return new ID(this.getBytes());
+        return key.getBytes();
     }
 
     @Override
     public String toString(){
-        String str = "";
-        for(int i=0; i<key.length; i++){
-            str = str + String.format("%02X ", key[i]);
-        }
-        return str; 
+        return key;
     }
 }
