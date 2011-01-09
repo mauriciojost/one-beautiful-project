9af84d96caae6a0d75cf83d62acfcc33892f6536
mauriciojost <mauriciojost@d51e7b54-0ae6-4b57-6df4-149d15c66451>
2011-01-09 10:52:06 +0000

    [code] Changed the way we access to the library. Now we parse its output (to avoid concurrence problems).

--- trunk/P2PChordExecutor/src/main/MyKey.java
+++ trunk/P2PChordExecutor/src/main/MyKey.java
@@ -3,14 +3,28 @@ import de.uniba.wiai.lspi.chord.data.ID;
 import de.uniba.wiai.lspi.chord.service.*;
 
 public class MyKey implements Key{
+    public final static int KEY_SIZE = 20;
     private byte[] key;
 
    
     public MyKey(byte[] rawkey){
         key = rawkey; 
     }
-    public MyKey(String key){
-        this.key = key.getBytes();
+    
+    public MyKey(String str_key){
+        this.key = new byte[KEY_SIZE];
+        if (str_key.length() >= KEY_SIZE){
+            System.out.println("Key string big enough.");
+            for (int i=0; i<KEY_SIZE;i++){
+                this.key[i] = (byte)str_key.charAt(i);
+            }
+        }else{
+            System.out.println("Key strign not big enough. Completing with zeros...");
+            for (int i=0; i<KEY_SIZE;i++){
+                this.key[i] = (i<str_key.length()?(byte)str_key.charAt(i):0);
+            }
+        }
+        
     }
     public byte[] getBytes() {
         return key;
